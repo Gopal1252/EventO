5efff4342980577fe5f6e3713372428f644c755b
Gopal1252 <guptagopal252@gmail.com>
2024-01-18 15:41:52 +0530

    adding back sanitize html and removing vercel.json

--- schemas.js
+++ schemas.js
@@ -1,37 +1,35 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
 
-// const BaseJoi = require('joi');
-// const sanitizeHtml = require('sanitize-html');
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
 
-// const extension = (joi) => ({
-//     type: 'string',
-//     base: joi.string(),
-//     messages: {
-//         'string '{{#label}} must not include HTML!'
-//     },
-//     rules: {
-//     {
-//             validate(value, helpers) {
-//                 const clean = sanitizeHtml(value, {
-//                     allowedTags: [],
-//                     allowedAttributes: {},
-//                 });
-//                 if (clean !== value) return helpers.error('string { value })
-//                 return clean;
-//             }
-//         }
-//     }
-// });
-
-// const Joi = BaseJoi.extend(extension)
+const Joi = BaseJoi.extend(extension)
 
 module.exports.eventSchema = Joi.object({
     event : Joi.object({
-      title : Joi.string().required(),//should be a string and is required
-      description : Joi.string().required(),
+      title : Joi.string().required().escapeHTML(),//should be a string and is required
+      description : Joi.string().required().escapeHTML(),
       price : Joi.number().required().min(0),//should be a number and is required
-      location : Joi.string().required(),
-      venue : Joi.string().required(),
+      location : Joi.string().required().escapeHTML(),
+      venue : Joi.string().required().escapeHTML(),
       date : Joi.date().required(),
       time : Joi.string().required(),
     //   image  : Joi.string().required(),
@@ -41,6 +39,6 @@ module.exports.eventSchema = Joi.object({
 
 module.exports.commentSchema = Joi.object({
     comment: Joi.object({
-        body : Joi.string().required()
+        body : Joi.string().required().escapeHTML()
     }).required()
 });
