a06a63ae3430ce876b1c37675f9d8d8e0aba4e81
Gopal1252 <guptagopal252@gmail.com>
2024-01-18 15:02:34 +0530

    removing sanitize-html

--- schemas.js
+++ schemas.js
@@ -1,35 +1,37 @@
-const BaseJoi = require('joi');
-const sanitizeHtml = require('sanitize-html');
+const Joi = require('joi');
 
-const extension = (joi) => ({
-    type: 'string',
-    base: joi.string(),
-    messages: {
-        'string.escapeHTML': '{{#label}} must not include HTML!'
-    },
-    rules: {
-        escapeHTML: {
-            validate(value, helpers) {
-                const clean = sanitizeHtml(value, {
-                    allowedTags: [],
-                    allowedAttributes: {},
-                });
-                if (clean !== value) return helpers.error('string.escapeHTML', { value })
-                return clean;
-            }
-        }
-    }
-});
+// const BaseJoi = require('joi');
+// const sanitizeHtml = require('sanitize-html');
 
-const Joi = BaseJoi.extend(extension)
+// const extension = (joi) => ({
+//     type: 'string',
+//     base: joi.string(),
+//     messages: {
+//         'string '{{#label}} must not include HTML!'
+//     },
+//     rules: {
+//     {
+//             validate(value, helpers) {
+//                 const clean = sanitizeHtml(value, {
+//                     allowedTags: [],
+//                     allowedAttributes: {},
+//                 });
+//                 if (clean !== value) return helpers.error('string { value })
+//                 return clean;
+//             }
+//         }
+//     }
+// });
+
+// const Joi = BaseJoi.extend(extension)
 
 module.exports.eventSchema = Joi.object({
     event : Joi.object({
-      title : Joi.string().required().escapeHTML(),//should be a string and is required
-      description : Joi.string().required().escapeHTML(),
+      title : Joi.string().required(),//should be a string and is required
+      description : Joi.string().required(),
       price : Joi.number().required().min(0),//should be a number and is required
-      location : Joi.string().required().escapeHTML(),
-      venue : Joi.string().required().escapeHTML(),
+      location : Joi.string().required(),
+      venue : Joi.string().required(),
       date : Joi.date().required(),
       time : Joi.string().required(),
     //   image  : Joi.string().required(),
@@ -39,6 +41,6 @@ module.exports.eventSchema = Joi.object({
 
 module.exports.commentSchema = Joi.object({
     comment: Joi.object({
-        body : Joi.string().required().escapeHTML()
+        body : Joi.string().required()
     }).required()
 });
